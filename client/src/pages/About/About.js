51b4d013a95127a277f97b6beacf3c63fe91219f
mc4tech <chavezmeguel@yahoo.com>
2017-10-14 12:12:21 -0400

    updated about

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -36,15 +36,15 @@ class Home extends Component {
                   <img className="img-circle " src="http://via.placeholder.com/140x140"></img>
                   <Row>
                     <a href="#" target="_blank" alt="msg"><img className="img-circle links" src="https://www.w3schools.com/css/trolltunga.jpg"></img></a>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links col-md-offset-2" src="../../../GitHub-Mark-64px.png"></img></a>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links col-md-offset-2" src="../../../linkedin-logo.png"></img></a>
+                    <a href="#" target="_blank" alt="msg"><img className="img-circle links col-md-offset-2" src="../GitHub-Mark-64px.png"></img></a>
+                    <a href="#" target="_blank" alt="msg"><img className="img-circle links col-md-offset-2" src="./linkedin.png"></img></a>
                   </Row>
                 </Col>
                 <Col size="md-3 md-offset-1">
                   <img className="img-circle " src="http://via.placeholder.com/140x140"></img>
                   <Row>
                     <a href="#" target="_blank" alt="msg"><img className="img-circle links" src="http://via.placeholder.com/25x25"></img></a>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links col-md-offset-2" src="../../../GitHub-Mark-64px.png"></img></a>
+                    <a href="#" target="_blank" alt="msg"><img className="img-circle links col-md-offset-2" src="../Images/GitHub-Mark-64px.png"></img></a>
                     <a href="#" target="_blank" alt="msg"><img className="img-circle links col-md-offset-2" src="../../../linkedin-logo.png"></img></a>
                   </Row>
                 </Col>
--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -52,48 +52,51 @@ class Dashboard extends Component {
 
   render() {
     return (
-      <Container fluid>
-        <Row>
-          <Col size="md-6">
-          
-              <h1>Please Complete Your Profile?</h1>
-      
-            <form>
-              <Input
-                value={this.state.username}
-                onChange={this.handleInputChange}
-                name="username"
-                placeholder="username (required)"
-              />
-              <Input
-                value={this.state.avgMileWalking}
-                onChange={this.handleInputChange}
-                name="avgMileWalking"
-                placeholder="Average Mile Walking (required)"
-              />
-              <Input
-                value={this.state.avgMileJogging}
-                onChange={this.handleInputChange}
-                name="avgMileJogging"
-                placeholder="Average Mile Jogging (optional)"
-              />
-              <Input
-                value={this.state.avgMileBiking}
-                onChange={this.handleInputChange}
-                name="avgMileBiking"
-                placeholder="Average Mile Biking (optional)"
-              />
-              <FormBtn
-                disabled={!(this.state.avgMileWalking && this.state.username)}
-                onClick={this.handleFormSubmit}
-              >
-                Submit
-              </FormBtn>
-            </form>
-          </Col>
-         
-        </Row>
-      </Container>
+      <div>
+        <Jumbotron/>
+        <Container fluid>
+          <Row>
+            <Col size="md-6">
+            
+                <h1>Please Complete Your Profile?</h1>
+        
+              <form>
+                <Input
+                  value={this.state.username}
+                  onChange={this.handleInputChange}
+                  name="username"
+                  placeholder="username (required)"
+                />
+                <Input
+                  value={this.state.avgMileWalking}
+                  onChange={this.handleInputChange}
+                  name="avgMileWalking"
+                  placeholder="Average Mile Walking (required)"
+                />
+                <Input
+                  value={this.state.avgMileJogging}
+                  onChange={this.handleInputChange}
+                  name="avgMileJogging"
+                  placeholder="Average Mile Jogging (optional)"
+                />
+                <Input
+                  value={this.state.avgMileBiking}
+                  onChange={this.handleInputChange}
+                  name="avgMileBiking"
+                  placeholder="Average Mile Biking (optional)"
+                />
+                <FormBtn
+                  disabled={!(this.state.avgMileWalking && this.state.username)}
+                  onClick={this.handleFormSubmit}
+                >
+                  Submit
+                </FormBtn>
+              </form>
+            </Col>
+           
+          </Row>
+        </Container>
+       </div>
     
       );
   }
