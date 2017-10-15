2fb483d4380849e2d086a267ae7b2497c03fc334
mc4tech <chavezmeguel@yahoo.com>
2017-10-15 00:51:27 -0400

    update about links/photos

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
-import Panel from "../../components/Panel";
-import API from "../../utils/API";
-import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn } from "../../components/Form";
-import DeleteBtn from "../../components/DeleteBtn";
 import Nav from "../../components/Nav";
 import Jumbotron from "../../components/Jumbotron";
 import "./About.css";
-
+import Safiyya from "./images/Safiyya.jpg";
+import Menelik from "./images/Menelik.jpg";
+import Linkedin from "./images/linkedin.png";
+import Github from "./images/GitHub-Mark-64px.png";
 
 
 class Home extends Component {
@@ -34,45 +31,50 @@ class Home extends Component {
             <div className="text-center">
               <Row>
                 <Col size="md-3 md-offset-1">
-                  <img className="img-circle " src="http://via.placeholder.com/140x140"></img>
+                   <h3>Safiyya Shariff</h3>
+                  <img className="img-circle bioPic" alt="Safiyya Shariff" src={Safiyya}></img>
                   <Row>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links" src="https://www.w3schools.com/css/trolltunga.jpg"></img></a>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links col-md-offset-2" src="../GitHub-Mark-64px.png"></img></a>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links col-md-offset-2" src="./linkedin.png"></img></a>
+                    <a href="#" target="_blank"><img className="img-circle links" src="https://www.w3schools.com/css/trolltunga.jpg"></img></a>
+                    <a href="https://github.com/ssharif10" target="_blank"><img className="img-circle links col-md-offset-2" alt="github" src={Github}></img></a>
+                    <a href="https://www.linkedin.com/in/safiyya-sharif-cpim" target="_blank"><img className="img-circle links col-md-offset-2" alt="linkedin" src={Linkedin}></img></a>
                   </Row>
                 </Col>
                 <Col size="md-3 md-offset-1">
-                  <img className="img-circle " src="http://via.placeholder.com/140x140"></img>
+                  <h3>Meguel Chavez</h3>
+                  <img className="img-circle bioPic" alt="Meguel Chavez" src="http://via.placeholder.com/140x140"></img>
                   <Row>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links" src="http://via.placeholder.com/25x25"></img></a>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links col-md-offset-2" src="../Images/GitHub-Mark-64px.png"></img></a>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links col-md-offset-2" src="../../../linkedin-logo.png"></img></a>
+                    <a href="#" target="_blank"><img className="img-circle links" src="http://via.placeholder.com/25x25"></img></a>
+                    <a href="https://github.com/mc4tech" target="_blank"><img className="img-circle links col-md-offset-2" alt="github" src={Github}></img></a>
+                    <a href="www.linkedin.com/in/meguel-chavez" target="_blank"><img className="img-circle links col-md-offset-2" alt="linkedin" src={Linkedin}></img></a>
                   </Row>
                 </Col>
                 <Col size="md-3 md-offset-1">
-                  <img className="img-circle " src="http://via.placeholder.com/140x140"></img>
+                  <h3>Menelik Tefera</h3>
+                  <img className="img-circle bioPic" alt="Menelik Tefera" src={Menelik}></img>
                   <Row>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links" src="http://via.placeholder.com/25x25"></img></a>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links col-md-offset-2" src="../../../GitHub-Mark-64px.png"></img></a>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links col-md-offset-2" src="../../../linkedin-logo.png"></img></a>
+                    <a href="#" target="_blank"><img className="img-circle links" src="http://via.placeholder.com/25x25"></img></a>
+                   <a href="https://github.com/menelik7" target="_blank"><img className="img-circle links col-md-offset-2" alt="github" src={Github}></img></a>
+                    <a href="#" target="_blank"><img className="img-circle links col-md-offset-2" alt="linkedin" src={Linkedin}></img></a>
                   </Row>
                 </Col>  
               </Row>
               <Row>
                 <Col size="md-3 md-offset-3">
-                  <img className="img-circle " src="http://via.placeholder.com/140x140"></img>
+                  <h3>Carlvis Jones</h3>
+                  <img className="img-circle bioPic" alt="Carlvis Jones" src="http://via.placeholder.com/140x140"></img>
                   <Row>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links" src="http://via.placeholder.com/25x25"></img></a>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links col-md-offset-2" src="../../../GitHub-Mark-64px.png"></img></a>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links col-md-offset-2" src="../../../linkedin-logo.png"></img></a>
+                    <a href="#" target="_blank"><img className="img-circle links" src="http://via.placeholder.com/25x25"></img></a>
+                   <a href="https://github.com/TangentLine" target="_blank"><img className="img-circle links col-md-offset-2" alt="github" src={Github}></img></a>
+                    <a href="#" target="_blank"><img className="img-circle links col-md-offset-2" alt="linkedin" src={Linkedin}></img></a>
                   </Row>
                 </Col>
                 <Col size="md-3 md-offset-1">
-                  <img className="img-circle " src="http://via.placeholder.com/140x140"></img>
+                  <h3>Ray Shin</h3>
+                  <img className="img-circle bioPic" alt="Ray Shin" src="http://via.placeholder.com/140x140"></img>
                   <Row>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links" src="http://via.placeholder.com/25x25"></img></a>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links col-md-offset-2" src="../../../GitHub-Mark-64px.png"></img></a>
-                    <a href="#" target="_blank" alt="msg"><img className="img-circle links col-md-offset-2" src="../../../linkedin-logo.png"></img></a>
+                    <a href="#" target="_blank"><img className="img-circle links" src="http://via.placeholder.com/25x25"></img></a>
+                   <a href="https://github.com/rayjshin" target="_blank"><img className="img-circle links col-md-offset-2" alt="github" src={Github}></img></a>
+                    <a href="https://www.linkedin.com/in/ray-shin-91176016/" target="_blank"><img className="img-circle links col-md-offset-2" alt="linkedin" src={Linkedin}></img></a>
                   </Row>
                 </Col> 
                  
